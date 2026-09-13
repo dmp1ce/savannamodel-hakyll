2d39234ede6f4e6dc60e4b09988ac52ce57fc628
David <>
2026-09-13 14:28:37 +0300

    Update semantic/ build system to Fomantic UI 2.9.4
    - Replace gulpfile.js, tasks/, and src/ with 2.9.4 versions (gulp 5 compatible)
    - Re-apply custom color palette in src/site/globals/site.variables
    - Simplify package.json to depend only on fomantic-ui (build plugins come
      transitively); removes old inquirer/gulp-json-editor top-level deps
    - Remove stale package-lock.json (regenerate after next successful install)

--- semantic/tasks/config/project/release.js
+++ semantic/tasks/config/project/release.js
@@ -2,41 +2,51 @@
          Release Config
 *******************************/
 
-var
-  requireDotFile = require('require-dot-file'),
-  config,
-  npmPackage,
-  version
-;
+const requireDotFile = require('require-dot-file');
 
+let
+    config,
+    npmPackage,
+    version,
+    revision,
+    versionInFileName,
+    includeVersionInFileName
+;
 
 /*******************************
          Derived Values
 *******************************/
 
-try {
-  config = requireDotFile('semantic.json');
-}
-catch(error) {}
-
+config = requireDotFile('semantic.json', process.cwd());
 
 try {
-  npmPackage = require('../../../package.json');
-}
-catch(error) {
-  // generate fake package
-  npmPackage = {
-    name: 'Unknown',
-    version: 'x.x'
-  };
+    npmPackage = require('../../../package.json'); // eslint-disable-line global-require
+} catch (error) {
+    // generate fake package
+    npmPackage = {
+        name: 'Unknown',
+        version: 'x.x',
+    };
 }
 
 // looks for version in config or package.json (whichever is available)
-version = (npmPackage && npmPackage.version !== undefined && npmPackage.name == 'fomantic-ui')
-  ? npmPackage.version
-  : config.version
-;
+version = npmPackage && npmPackage.version !== undefined && npmPackage.name === 'fomantic-ui'
+    ? npmPackage.version
+    : config.version;
 
+// looks for revision in config.
+revision = config.revision === undefined ? '' : config.revision;
+
+includeVersionInFileName = config.includeVersionInFileName === undefined ? false : config.includeVersionInFileName;
+
+versionInFileName = '';
+
+if (includeVersionInFileName) {
+    versionInFileName = '-' + version;
+    if (revision !== '') {
+        versionInFileName += '-' + revision;
+    }
+}
 
 /*******************************
              Export
@@ -44,22 +54,23 @@ version = (npmPackage && npmPackage.version !== undefined && npmPackage.name == 
 
 module.exports = {
 
-  title      : 'Fomantic UI',
-  repository : 'https://github.com/fomantic/Fomantic-UI',
-  url        : 'http://fomantic-ui.com/',
+    title: 'Fomantic UI',
+    repository: 'https://github.com/fomantic/Fomantic-UI',
+    url: 'https://fomantic-ui.com/',
 
-  banner: ''
-    + ' /*' + '\n'
-    + ' * # <%= title %> - <%= version %>' + '\n'
-    + ' * <%= repository %>' + '\n'
-    + ' * <%= url %>' + '\n'
-    + ' *' + '\n'
-    + ' * Copyright 2014 Contributors' + '\n'
-    + ' * Released under the MIT license' + '\n'
-    + ' * http://opensource.org/licenses/MIT' + '\n'
-    + ' *' + '\n'
-    + ' */' + '\n',
+    banner: ''
+        + '/*\n'
+        + ' * # <%= title %> - <%= version %>\n'
+        + ' * <%= repository %>\n'
+        + ' * <%= url %>\n'
+        + ' *\n'
+        + ' * Copyright <%= year %> Contributors\n'
+        + ' * Released under the MIT license\n'
+        + ' * https://opensource.org/licenses/MIT\n'
+        + ' *\n'
+        + ' */\n',
 
-  version    : version
+    version: version,
+    versionInFileName: versionInFileName,
 
 };
